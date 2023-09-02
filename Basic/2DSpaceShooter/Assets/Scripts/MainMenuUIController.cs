e1b936e23d663ae24d122c5be3f8e4de412d96b7
CypherTrix <117116831+CypherTrix@users.noreply.github.com>
2023-09-02 21:32:39 +0200

    player stats main menu and settings

--- Basic/2DSpaceShooter/Assets/Scripts/MainMenuUIController.cs
+++ Basic/2DSpaceShooter/Assets/Scripts/MainMenuUIController.cs
@@ -1,11 +1,10 @@
+using System;
 using UnityEngine;
-using UnityEngine.UIElements;
-using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class MainMenuUIController : MonoBehaviour {
-    [Header("Mute Audio Button")]
-    [SerializeField] AudioMixer audioMixer;
 
     [Header("UI Elements")]
     private UIDocument mainDoc;
@@ -16,12 +15,33 @@ public class MainMenuUIController : MonoBehaviour {
     private Button settingsBtn;
     private Button exitBtn;
     //Settings
-    private VisualElement settingsMenu;
-    private Button backBtnSettings;
-
-
     [Header("Settings")]
-    private float masterVolume = 1f;
+    [SerializeField] AudioMixer audioMixer;
+    private VisualElement settingsMenu;
+    private Slider masterVolume;
+    private Label masterVolumeLabel;
+    private const string MASTER_VOLUME = "Master_Volume";
+    private Slider masterMusic;
+    private Label masterMusicLabel;
+    private const string MASTER_MUSIC = "Master_Music";
+    private Slider masterSound;
+    private Label masterSoundLabel;
+    private const string MASTER_SOUND = "Master_Sound";
+    private RadioButtonGroup graphicsSelection;
+    private const string GRAPHIC_QUALITY = "Graphic_Quality";
+    //Profil
+    private VisualElement profilMenu;
+    private Label playerStats;
+    private Button resetPlayer;
+    private VisualElement playerShipImage;
+    private VisualElement playerShipColor;
+    private EnumField enumColors;
+    private const string PLAYER_COLOR = "Player_Color";
+    private TextField playerName;
+    private const string PLAYER_NAME = "Player_Name";
+
+
+
     private void Awake() {
         mainDoc = GetComponent<UIDocument>();
         //MainMenu
@@ -35,18 +55,64 @@ public class MainMenuUIController : MonoBehaviour {
         exitBtn.clicked += ExitBtn_clicked;
         //Settings Menu
         settingsMenu = mainDoc.rootVisualElement.Q<VisualElement>("SettingsMenu");
-        backBtnSettings = mainDoc.rootVisualElement.Q<Button>("BtnBack");
-        backBtnSettings.clicked += BackBtnSettings_clicked;
+        masterVolume = mainDoc.rootVisualElement.Q<Slider>("SliderMasterVolume");
+        masterVolumeLabel = mainDoc.rootVisualElement.Q<Label>("LblMasterVolume");
+        masterMusic = mainDoc.rootVisualElement.Q<Slider>("SliderMusicVolume");
+        masterMusicLabel = mainDoc.rootVisualElement.Q<Label>("LblMusicVolume");
+        masterSound = mainDoc.rootVisualElement.Q<Slider>("SliderSoundVolume");
+        masterSoundLabel = mainDoc.rootVisualElement.Q<Label>("LblSoundVolume");
+        resetPlayer = mainDoc.rootVisualElement.Q<Button>("ResetPlayer");
+        resetPlayer.clicked += ResetPlayer_onClick;
+        graphicsSelection = mainDoc.rootVisualElement.Q<RadioButtonGroup>("GraphicsSelection");
+
+        //Profil Menu
+        profilMenu = mainDoc.rootVisualElement.Q<VisualElement>("ProfilMenu");
+        playerStats = mainDoc.rootVisualElement.Q<Label>("PlayerStats");
+
+        playerShipColor = mainDoc.rootVisualElement.Q<VisualElement>("PlayerShipColor");
+        playerShipImage = mainDoc.rootVisualElement.Q<VisualElement>("PlayerShipImage");
+        enumColors = mainDoc.rootVisualElement.Q<EnumField>("ColorsEnum");
+        playerName = mainDoc.rootVisualElement.Q<TextField>("PlayerName");
 
 
-        profilBtn.SetEnabled(false);
+
     }
 
-    private void BackBtnSettings_clicked() {
-        settingsMenu.style.display = DisplayStyle.None;
-        settingsBtn.SetEnabled(true);
+    private void ResetPlayer_onClick() {
+        PlayerPrefs.DeleteAll();
     }
 
+    private void Start() {
+        //Apply Settings to UI ELements
+        masterVolume.RegisterValueChangedCallback(evt => masterVolumeLabel.text = $"{Mathf.FloorToInt(evt.newValue * 100)} %");
+        masterVolume.RegisterValueChangedCallback(evt => audioMixer.SetFloat(MASTER_VOLUME, Mathf.Log(evt.newValue) * 20));
+        masterVolume.RegisterValueChangedCallback(evt => PlayerPrefs.SetFloat(MASTER_VOLUME, evt.newValue));
+        masterVolume.value = PlayerPrefs.HasKey(MASTER_VOLUME) ? PlayerPrefs.GetFloat(MASTER_VOLUME) : 1;
+
+        masterMusic.RegisterValueChangedCallback(evt => masterMusicLabel.text = $"{Mathf.FloorToInt(evt.newValue * 100)} %");
+        masterMusic.RegisterValueChangedCallback(evt => audioMixer.SetFloat(MASTER_MUSIC, Mathf.Log(evt.newValue) * 20));
+        masterMusic.RegisterValueChangedCallback(evt => PlayerPrefs.SetFloat(MASTER_MUSIC, evt.newValue));
+        masterMusic.value = PlayerPrefs.HasKey(MASTER_MUSIC) ? PlayerPrefs.GetFloat(MASTER_MUSIC) : 1;
+
+        masterSound.RegisterValueChangedCallback(evt => masterSoundLabel.text = $"{Mathf.FloorToInt(evt.newValue * 100)} %");
+        masterSound.RegisterValueChangedCallback(evt => audioMixer.SetFloat(MASTER_SOUND, Mathf.Log(evt.newValue) * 20));
+        masterSound.RegisterValueChangedCallback(evt => PlayerPrefs.SetFloat(MASTER_SOUND, evt.newValue));
+        masterSound.value = PlayerPrefs.HasKey(MASTER_SOUND) ? PlayerPrefs.GetFloat(MASTER_SOUND) : 1;
+
+        graphicsSelection.SetValueWithoutNotify(PlayerPrefs.HasKey(GRAPHIC_QUALITY) ? PlayerPrefs.GetInt(GRAPHIC_QUALITY) : 1); //Default Level 'Normal'
+        graphicsSelection.RegisterValueChangedCallback(evt => QualitySettings.SetQualityLevel(evt.newValue));
+        graphicsSelection.RegisterValueChangedCallback(evt => PlayerPrefs.SetInt(GRAPHIC_QUALITY, evt.newValue));
+
+        //Profil
+
+        enumColors.RegisterValueChangedCallback(evt => playerShipColor.style.unityBackgroundImageTintColor = PlayerColors.ToColor(evt.newValue.ToString()));
+        enumColors.RegisterValueChangedCallback(evt => PlayerPrefs.SetInt(PLAYER_COLOR, (int)(PlayerColors.PlayerColor)evt.newValue));
+        enumColors.RegisterValueChangedCallback(evt => Debug.Log( (int)(PlayerColors.PlayerColor)evt.newValue));
+        enumColors.value = PlayerPrefs.HasKey(PLAYER_COLOR) ? (PlayerColors.PlayerColor)PlayerPrefs.GetInt(PLAYER_COLOR) : PlayerColors.PlayerColor.White;
+
+        playerName.RegisterValueChangedCallback(evt => PlayerPrefs.SetString(PLAYER_NAME,evt.newValue));
+        playerName.value = PlayerPrefs.HasKey(PLAYER_NAME) ? PlayerPrefs.GetString(PLAYER_NAME) : Environment.UserName;
+    }
 
     #region MainMenu
     private void PlayBtn_clicked() {
@@ -54,11 +120,13 @@ public class MainMenuUIController : MonoBehaviour {
     }
 
     private void ProfilBtn_clicked() {
+        settingsMenu.style.display = DisplayStyle.None;
+        profilMenu.style.display = profilMenu.style.display == DisplayStyle.Flex ? DisplayStyle.None : DisplayStyle.Flex;
 
     }
     private void SettingsBtn_clicked() {
-        settingsMenu.style.display = DisplayStyle.Flex;
-        settingsBtn.SetEnabled(false);
+        profilMenu.style.display = DisplayStyle.None;
+        settingsMenu.style.display = settingsMenu.style.display == DisplayStyle.Flex ? DisplayStyle.None : DisplayStyle.Flex;
     }
     private void ExitBtn_clicked() {
 #if UNITY_EDITOR
@@ -70,12 +138,12 @@ public class MainMenuUIController : MonoBehaviour {
 
     #region Settings
     private void MuteBtn_onClick() {
-       //isMuted = !isMuted;
-       // StyleBackground bg = muteBtn.style.backgroundImage;
-       // bg.value = Background.FromSprite(isMuted ? muteSprite : unmuteSprite);
-       // muteBtn.style.backgroundImage = bg;
+        //isMuted = !isMuted;
+        // StyleBackground bg = muteBtn.style.backgroundImage;
+        // bg.value = Background.FromSprite(isMuted ? muteSprite : unmuteSprite);
+        // muteBtn.style.backgroundImage = bg;
 
-       // audioMixer.SetFloat("Master_Volume", isMuted ? -80 : 0);
+        // audioMixer.SetFloat("Master_Volume", isMuted ? -80 : 0);
     }
     #endregion
 
---
+++ Basic/2DSpaceShooter/Assets/Scripts/PlayerColors.cs
@@ -0,0 +1,21 @@
+﻿
+
+using UnityEngine;
+
+public static class PlayerColors {
+    public enum PlayerColor : int {
+        White = 0,
+        Black = 1,
+        Red = 2,
+        Green = 3,
+        Blue = 4,
+        Yellow = 5,
+        Gray = 6,
+        Magenta = 7,
+        Cyan = 8
+
+    }
+    public static Color ToColor(this string color) {
+        return (Color)typeof(Color).GetProperty(color.ToLowerInvariant()).GetValue(null, null);
+    }
+}
